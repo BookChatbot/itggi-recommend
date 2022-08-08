536b4d569702f8d38bd268316b9eb5fd14c49a75
Jun <fohens295@gmail.com>
2022-08-08 19:39:48 +0900

    바로 db에 업데이트

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-from cb import get_cosine_similarity, get_document_vectors, recommendations
+from cb import get_cosine_similarity, recommendations
 from cf import get_similar_by_cf, predict_rating
 from log import info_log, error_log
 import pandas as pd
 from datetime import datetime
-from db import update_similarity
+from db import update_similarity, get_pd_from_table
 
 now = datetime.now()
-if now.hour == 23:
+if now.hour == 3:
     # Contents Based 추천 시스템
     try:
-        cosine_similarities, books, word2vec_model = get_cosine_similarity()
-        document_embedding_list = get_document_vectors(
-            books['summary'], word2vec_model)
-        books.drop(['summary'], axis=1, inplace=True)
+        books = get_pd_from_table('books')
+        books.drop(['isbn', 'pubDate', 'img', 'rate', 'bestseller'], axis=1, inplace=True)
+        books.fillna('', inplace=True)
+        # 책 소개 글에 제목,저자,출판사,장르 추가
+        summaries = []
+        for book in books.values:
+            summary = book[1]
+            summary += book[2]
+            summary += book[3]
+            summary += book[4]
+            summary += book[5]
+            summaries.append(summary)
+        books['summary'] = summaries
+        books.drop(['title', 'author', 'publisher', 'genre'], axis=1, inplace=True)
 
-        for book_id in books['id']:
-            rec_books = recommendations(books, book_id, cosine_similarities).id
+        cosine_similarity = get_cosine_similarity(books)
+        info_log('cb 추천 모델을 업데이트 후 저장했습니다.')
+
+        # similarity 값이 비어있는 경우만 업데이트
+        non_similar_books = books[books['similarity'] == ''].index
+        for book_id in non_similar_books:
+            rec_books = recommendations(book_id).id 
             rec_books = rec_books.to_list()
             rec_books = [str(i) for i in rec_books]
             rec_books = ",".join(rec_books)
             update_similarity('books', book_id, rec_books)
-        info_log('cb 추천이 업데이트 되었습니다.')
+        info_log('cb 유사도 업데이트 했습니다.')
     except Exception as e:
         error_log(e)
 else:
