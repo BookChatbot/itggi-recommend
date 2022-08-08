e10dc2a7a2efbc5a280090067b7c6ba983cb8599
Jun <fohens295@gmail.com>
2022-08-08 20:15:42 +0900

    파이썬 3.9부터 encoding 사용 가능

--- log.py
+++ log.py
@@ -3,7 +3,7 @@ import datetime
 from pytz import timezone
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO, encoding='utf-8', filename='log/recommendation.log')
+                    level=logging.INFO, filename='log/recommendation.log')
 
 
 def info_log(message):
